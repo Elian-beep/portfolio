0449eab69555b302cb29e7e60c58f7d65ca77dc1
Elian-beep <elian.19batista@gmail.com>
2025-04-26 11:51:21 -0400

    feat: add irede

--- src/mocks/Jobs.ts
+++ src/mocks/Jobs.ts
@@ -1,12 +1,21 @@
 import icon_bd from '@/assets/icons/icon_bd.png';
 import icon_uea from '@/assets/icons/icon_uea.png';
+import icon_irede from '@/assets/icons/icon_irede.jpg';
 
 export const jobs = [
     {
-        id: 1,
+        id: 4,
+        name: 'iRede',
+        role: 'Desenvolvedor Frontend',
+        period: 'Dez/2024 - Atualmente',
+        image: icon_irede,
+        desc: 'Desenvolvimento e manutenção de projetos P&D visando a construção de conhecimento sobre temas específicos, através de investigação e experimentação para a criação de novos produtos ou serviços',
+    },
+    {
+        id: 3,
         name: 'Bemol Digital',
         role: 'Desenvolvedor Trainee',
-        period: 'mar/2024-Atualmente',
+        period: 'mar/2024 - Dez/2024',
         image: icon_bd,
         desc: 'Desenvolvimento de novos projetos e manutenção dos sofwatres referentes referentes aos sistemas de loja da empresa de Varejo Bemol S/A',
     },
